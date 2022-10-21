f12dc4b67c050fc60857e69c1e3cb883142736ee
Firas Debich <firasdebich06@gmail.com>
2022-10-21 10:41:13 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { EnvironmentsEnum } from '@elrondnetwork/dapp-core/types';
+import {
+  TransactionsToastList,
+  SignTransactionsModals,
+  NotificationModal
+} from '@elrondnetwork/dapp-core/UI';
+import { DappProvider } from '@elrondnetwork/dapp-core/wrappers';
+
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import Layout from 'components/Layout';
+import { walletConnectV2ProjectId } from 'config';
+import PageNotFound from 'pages/PageNotFound';
+import { routeNames } from 'routes';
+import routes from 'routes';
+import UnlockPage from './pages/UnlockPage';
+
+const environment = EnvironmentsEnum.devnet;
+
+const App = () => {
+  return (
+    <Router>
+      <DappProvider
+        environment={environment}
+        customNetworkConfig={{
+          name: 'customConfig',
+          apiTimeout: 6000,
+          walletConnectV2ProjectId
+        }}
+      >
+        <Layout>
+          <TransactionsToastList />
+          <NotificationModal />
+          <SignTransactionsModals className='custom-class-for-modals' />
+          <Routes>
+            <Route path={routeNames.unlock} element={<UnlockPage />} />
+            {routes.map((route: any, index: number) => (
+              <Route
+                path={route.path}
+                key={'route-key-' + index}
+                element={<route.component />}
+              />
+            ))}
+            <Route path='*' element={<PageNotFound />} />
+          </Routes>
+        </Layout>
+      </DappProvider>
+    </Router>
+  );
+};
+
+export default App;
